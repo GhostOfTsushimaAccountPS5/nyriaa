9f5dce72a0b0d099fb030d67ce7cec79f2b16f8a
Nyriaa <nyriaa@github.com>
2026-01-31 20:01:08 +0100

    Add website files

---
+++ outfits.js
@@ -0,0 +1,142 @@
+const outfits = [
+  {
+    id: "party-1",
+    title: "🤍 PARTY ・ OUTFIT 🤍",
+    image: "images/liste-outfit/party-1.png",
+    author: "@sofiaprc",
+    badge: "creator",
+
+    items: [
+      {
+        name: "Robe pailletée",
+        brand: "Shein",
+        image: "haut-1.png",
+        link: "https://euqs.shein.com/Glamine-Deep-V-Sequin-Embellished-Mini-Dress-p-48516843.html"
+      },
+      {
+        name: "Talons hauts mi-mollet",
+        brand: "Shein",
+        image: "botte-1.png",
+        link: "https://euqs.shein.com/goods-p-129215002.html"
+      },
+      {
+        name: "Sac à bandoulière en chaîne",
+        brand: "Amazon",
+        image: "sac-1.png",
+
+        affiliate: {
+          fr: "https://www.amazon.fr/dp/B0FGX9175Q?tag=sofiaprc-21",
+          be: "https://www.amazon.com.be/dp/B0FGX9175Q?tag=sofiaprcbe-21"
+        }
+      },
+      {
+        name: "Collier en acier",
+        brand: "Amazon",
+        image: "collier-1.png",
+
+        affiliate: {
+          fr: "https://www.amazon.fr/dp/B0DPNBNVHZ?tag=sofiaprc-21",
+          be: "https://www.amazon.com.be/dp/B0DPNBNVHZ?tag=sofiaprcbe-21"
+        }
+      }
+    ]
+  },
+  {
+    id: "y2kblack-1",
+    title: "🖤 Y2K BLACK ・ OUTFIT 🖤",
+    image: "images/liste-outfit/y2kblack-1.png",
+    author: "@sofiaprc",
+    badge: "creator",
+
+    items: [
+      {
+        name: "Black - Gillet",
+        brand: "Zalando",
+        image: "haut-1.png",
+        link: "https://www.zalando.fr/evenandodd-gilet-black-ev421d2nx-q11.html"
+      },
+      {
+        name: "Mini jupe crayon stretch",
+        brand: "Amazon",
+        image: "jupe-1.png",
+
+        affiliate: {
+          fr: "https://www.amazon.fr/DGSHIRLDJO-Mini-crayon-stretch-femme/dp/B0CN2L85MW?tag=sofiaprc-21",
+          be: "https://www.amazon.com.be/DGSHIRLDJO-Mini-crayon-stretch-femme/dp/B0CN2L85MW?tag=sofiaprcbe-21"
+        }
+      },
+      {
+        name: "SANTIAGS - Bottes de cowboy",
+        brand: "Zalando",
+        image: "botte-1.png",
+        link: "https://www.zalando.fr/super-mode-bottes-de-cowboy-motard-taupe-sjz11n07p-b11.html"
+      },
+    ]
+  },
+  {
+    id: "girlwinter-1",
+    title: "🧊 GIRLWINTER ・ OUTFIT 🧊",
+    image: "images/liste-outfit/girlwinter-1.png",
+    author: "@sofiaprc",
+    badge: "creator",
+
+    items: [
+      {
+        name: "MOSS COPENHAGEN BATHILDA",
+        brand: "Zalando",
+        image: "haut-1.png",
+        link: "https://fr.zalando.be/msch-copenhagen-moss-copenhagen-bathilda-pullover-hellgrau-m0y21i09v-c11.html"
+      },
+      {
+        name: "SMITH 2 - Bottes à talons",
+        brand: "Zalondo",
+        image: "botte-1.png",
+        link: "https://fr.zalando.be/steve-madden-smith-2-bottes-a-talons-hauts-black-st311n0h9-q11.html",
+      },
+      {
+        name: "Mini jupe crayon stretch",
+        brand: "Amazon",
+        image: "jupe-1.png",
+
+        affiliate: {
+          fr: "https://www.amazon.fr/DGSHIRLDJO-Mini-crayon-stretch-femme/dp/B0CN2L85MW?tag=sofiaprc-21",
+          be: "https://www.amazon.com.be/DGSHIRLDJO-Mini-crayon-stretch-femme/dp/B0CN2L85MW?tag=sofiaprcbe-21"
+        }
+      },
+      {
+        name: "Sac à main - black",
+        brand: "Zalondo",
+        image: "sac-1.png",
+        link: "https://fr.zalando.be/liu-jo-sac-bandouliere-black-li651h2ou-q11.html",
+      },
+    ]
+  },
+  {
+    id: "leopard-1",
+    title: "🤎 LEOPARD ・ OUTFIT 🤎",
+    image: "images/liste-outfit/leopard-1.png",
+    author: "@sofiaprc",
+    badge: "creator",
+
+    items: [
+      {
+        name: "Top à manches longues léopard",
+        brand: "Shein",
+        image: "haut-1.png",
+        link: "https://euqs.shein.com/SHEIN-EZwear-Women-s-Leopard-Print-Asymmetric-Neckline-Ruched-Fitted-Long-Sleeve-Top-p-46719797.html"
+      },
+      {
+        name: "Short Mini Noir À Paillettes",
+        brand: "Shein",
+        image: "jupe-1.png",
+        link: "https://m.shein.com/fr/ROMWE-Grunge-Punk-Women-s-Black-Sequin-Punk-Mini-Shorts-Sexy-Ultra-Low-Waist-Micro-Shorts-For-Summer-Beach-Graduation-Easter-Concert-Nashville-Vacation-Music-Festival-p-55424094.html",
+      },
+      {
+        name: "SANTIAGS - Bottes de cowboy",
+        brand: "Zalando",
+        image: "botte-1.png",
+        link: "https://www.zalando.fr/super-mode-bottes-de-cowboy-motard-taupe-sjz11n07p-b11.html"
+      }
+    ]
+  }
+];
